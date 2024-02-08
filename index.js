c573f71ca3bba01a8e0d441badbcf242f047b03b
Sigazen <choikdee50@gmail.com>
2024-02-08 21:10:41 +0700

    Add project

---
+++ index.js
@@ -0,0 +1,200 @@
+/* eslint-disable indent */
+/* eslint-disable keyword-spacing */
+/* eslint-disable comma-spacing */
+/* eslint-disable space-before-blocks */
+'use strict';
+const line = require('@line/bot-sdk');
+const express = require('express');
+const config = require('./config.json');
+
+// create LINE SDK client
+const client = new line.messagingApi.MessagingApiClient(config);
+
+const app = express();
+
+let queue = [];
+
+// webhook callback
+app.post('/webhook', line.middleware(config), (req, res) => {
+  if (!Array.isArray(req.body.events)) {
+    return res.status(500).end();
+  }
+
+  Promise.all(req.body.events.map(event => {
+    console.log('event', event);
+    return handleEvent(event);
+  }))
+  .then(() => res.end())
+  .catch((err) => {
+    console.error(err);
+    res.status(500).end();
+  });
+});
+
+// simple reply function
+const replyText = (replyToken, text, quoteToken) => {
+  return client.replyMessage({
+    replyToken,
+    messages: [{
+      type: 'text',
+      text,
+      quoteToken,
+    }],
+  });
+};
+
+const replyTextandSticker = (replyToken, text, quoteToken,stickerId,packageId) => {
+
+  const stickerMessage = {
+    type: 'sticker',
+    packageId: packageId,
+    stickerId: stickerId,
+  };
+
+  return client.replyMessage({
+    replyToken,
+    messages: [{
+      type: 'text',
+      text,
+      quoteToken,
+    },stickerMessage],
+  });
+};
+
+// callback function to handle a single event
+function handleEvent(event) {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    // Non-text message, ignore
+    return Promise.resolve(null);
+  }
+
+  // Extract user ID from the event
+  const userId = event.source.userId;
+  // Retrieve user profile information
+  if(event.message.text === 'ตรวจสอบและเพิ่มเข้าคิว'){
+    if (!queue.includes(userId)) {
+      queue.push(userId);
+      return client.getProfile(userId)
+      .then((profile) => {
+        const displayName = profile.displayName;
+        var text = 'เราได้เพิ่มคุณ ';
+        text += displayName;
+        text += ' เข้าสู่คิวการใช้บริการเรียบร้อย';
+        text += '\nคิวของคุณคือคิวที่ ';
+        text += queue.length;
+        replyTextandSticker(event.replyToken,text,event.quoteToken,16581266,8522);
+        // Handle the user's display name
+      })
+      .catch((err) => {
+        console.error('Error fetching user profile:', err);
+        // Handle error
+      });
+    } else{
+      return client.getProfile(userId)
+      .then((profile) => {
+        const displayName = profile.displayName;
+        var text = 'คุณ ';
+        text += displayName;
+        text += ' ได้อยู่ในคิวแล้ว';
+        text += '\nคิวของคุณคือคิวที่ ';
+        text += queue.indexOf(userId) + 1;
+        replyText(event.replyToken,text,event.quoteToken);
+        // Handle the user's display name
+      })
+      .catch((err) => {
+        console.error('Error fetching user profile:', err);
+        // Handle error
+      });
+    }
+  }
+  if(event.message.text === 'ตรวจสอบคิวและเวลา'){
+    if (!queue.includes(userId)) {
+      queue.push(userId);
+      return client.getProfile(userId)
+      .then((profile) => {
+        var text = 'คุณยังไม่ได้อยู่ในคิว\nหากต้องการเพิ่มคิวเข้าใช้งานเครื่องซักผ้า สามารถกดเมนู"จองคิว"\nหรือพิมพ์ "ตรวจสอบและเพิ่มเข้าคิว" ได้เลยครับ';
+        replyText(event.replyToken,text,event.quoteToken);
+        // Handle the user's display name
+      })
+      .catch((err) => {
+        console.error('Error fetching user profile:', err);
+        // Handle error
+      });
+    } else{
+      return client.getProfile(userId)
+      .then((profile) => {
+        const displayName = profile.displayName;
+        var text = 'คุณ ';
+        text += displayName;
+        text += ' ได้อยู่ในคิวแล้ว';
+        text += '\nคิวของคุณคือคิวที่ ';
+        text += queue.indexOf(userId) + 1;
+        replyText(event.replyToken,text,event.quoteToken);
+        // Handle the user's display name
+      })
+      .catch((err) => {
+        console.error('Error fetching user profile:', err);
+        // Handle error
+      });
+    }
+  }
+  if(event.message.text === 'ยกเลิกคิว'){
+    if (!queue.includes(userId)) {
+      queue.push(userId);
+      return client.getProfile(userId)
+      .then((profile) => {
+        var text = 'คุณยังไม่ได้อยู่ในคิว\nหากต้องการเพิ่มคิวเข้าใช้งานเครื่องซักผ้า สามารถกดเมนู"จองคิว"\nหรือพิมพ์ "ตรวจสอบและเพิ่มเข้าคิว" ได้เลยครับ';
+        replyText(event.replyToken,text,event.quoteToken);
+        // Handle the user's display name
+      })
+      .catch((err) => {
+        console.error('Error fetching user profile:', err);
+        // Handle error
+      });
+    } else{
+      return client.getProfile(userId)
+      .then((profile) => {
+        var text = 'คุณแน่ใจหรือไม่ว่าต้องการจะยกเลิกการจองคิว\nหากยืนยันพิมพ์"ยืนยัน"';
+        replyText(event.replyToken,text,event.quoteToken);
+        // Handle the user's display name
+      })
+      .catch((err) => {
+        console.error('Error fetching user profile:', err);
+        // Handle error
+      });
+    }
+  }
+  if(event.message.text === 'ยืนยัน'){
+    if (!queue.includes(userId)) {
+      queue.push(userId);
+      return client.getProfile(userId)
+      .then((profile) => {
+        var text = 'คุณยังไม่ได้อยู่ในคิว\nหากต้องการเพิ่มคิวเข้าใช้งานเครื่องซักผ้า สามารถกดเมนู"จองคิว"\nหรือพิมพ์ "ตรวจสอบและเพิ่มเข้าคิว" ได้เลยครับ';
+        replyText(event.replyToken,text,event.quoteToken);
+        // Handle the user's display name
+      })
+      .catch((err) => {
+        console.error('Error fetching user profile:', err);
+        // Handle error
+      });
+    } else{
+      return client.getProfile(userId)
+      .then((profile) => {
+        const index = queue.indexOf(userId);
+        if (index !== -1) {
+          queue.splice(index, 1);
+        }
+        var text = 'คิวของคุณได้ถูกลบเรียบร้อย\nขอบคุณที่ใช้บริการครับ';
+        replyTextandSticker(event.replyToken,text,event.quoteToken,52002739,11537);
+      })
+      .catch((err) => {
+        console.error('Error fetching user profile:', err);
+        // Handle error
+      });
+    }
+  }
+}
+const port = config.port;
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
+});
